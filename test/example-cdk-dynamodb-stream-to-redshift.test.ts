81dfb0c83ca0faf84270c84e313ee0e801e44226
David McElligott <dmmcelli@amazon.com>
2022-05-12 09:33:58 -0500

    Initial commit

---
+++ test/example-cdk-dynamodb-stream-to-redshift.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ExampleCdkDynamodbStreamToRedshift from '../lib/example-cdk-dynamodb-stream-to-redshift-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/example-cdk-dynamodb-stream-to-redshift-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ExampleCdkDynamodbStreamToRedshift.ExampleCdkDynamodbStreamToRedshiftStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
